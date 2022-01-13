1f79da3e533f51cf0db208cd55ed5a0df138d26c
SatansLeftNipple <92038782+SatansLeftNipple@users.noreply.github.com>
2022-01-13 17:47:55 +0100

    Update Damspel.py

--- Damspel.py
+++ Damspel.py
@@ -39,14 +39,15 @@ class Spelbräda:
         return matris_spelbräda
 
     def __str__(self):
-        raden = "  "
+        raden = "   "
         #lägger till den första raden som visar kolonners nummer
         for counter in range(1, self.matrisstorlek+1):
-            raden += str(counter) + " "
+            raden += "{0:<3}".format(str(counter))
         # index räknas upp genom varje iteration och läggs till som det första numret till varje rad
         for index, rad in enumerate (self.spelbräda, start=1):
-            raden += "\n" + str(index)+ " " + (((" ".join(map(str, rad))).replace("1", self.symbol1)).replace("2", self.symbol2)).replace("0", "-") 
+            raden += "\n" + "{0:<3s}".format(str(index)) + (((((("  ".join(map(str, rad))).replace("10", "W")).replace("20", "L")).replace("1", self.symbol1)).replace("2", self.symbol2)).replace("0", "-"))
         return raden + "\n"
+
 
     def flytta_pjäs(self, vald_pjäs_koordinater, vald_plats_koordinater):
         """kanke kör denna som bara skapar en ny punkt med ett värde i matrisen och inte ändrar något gammal
@@ -54,14 +55,16 @@ class Spelbräda:
             input:- output: matris """ 
         self.spelbräda[vald_plats_koordinater[0]][vald_plats_koordinater[1]] = self.spelbräda[vald_pjäs_koordinater[0]][vald_pjäs_koordinater[1]]
         self.spelbräda[vald_pjäs_koordinater[0]][vald_pjäs_koordinater[1]] = 0
-        return self.spelbräda
+
+    def ta_pjäs(self, pjäs, plats, attackerad_pjäs):
+
+        self.spelbräda[plats[0]][plats[1]] = self.spelbräda[pjäs[0]][pjäs[1]]
+        self.spelbräda[pjäs[0]][pjäs[1]] = 0
+        self.spelbräda[attackerad_pjäs[0]][attackerad_pjäs[1]] = 0
+
 
 
 class Spela(Spelbräda):
-
-#    def granskaspelplan(self):
-        #kollar efter lagliga drag
-
 
     def kolla_ifall_vald_pjäs_finns(self, pjäs, attackerad_pjäs=[0,0]):
         #kollar ifall vald_pjäs anger en position på brädan där det antingen står 1 eller 2 alltså in 0
@@ -120,22 +123,32 @@ class Spela(Spelbräda):
         if 3 > abs(pjäs[0]-plats[0]) > 1 and self.kolla_ifall_vald_pjäs_finns(pjäs, attackerad_pjäs) == True:
             return True, attackerad_pjäs
         else:
-            return False
+            return False, ""
 
     def kolla_laglilg_sträcka_flytta(self, pjäs, plats):
         if 2 > abs(pjäs[0]-plats[0]) > 0:
             return True 
         else:
             return False
+    
+    def kolla_laglig_rikting(self, pjäs, plats):
+        #funktion kollar så att pjäser rör sig åt rätt håll
+        typ_pjäs = self.spelbräda[pjäs[0]][pjäs[1]]
+        if typ_pjäs == 1 and (pjäs[0] - plats[0]) > 0:
+            return False
+        elif typ_pjäs == 2 and (pjäs[0] - plats[0]) < 0:
+            return False
+        else:
+            return True
         
     def kolla_obligatoriska_drag_höger_ner(self):
         # index1 tillhör rad och index2 tillhör kolonn
         for index1, a in enumerate (self.spelbräda):
             for index2, i in enumerate (a):
                 #Try satsen ser till så att ifall index går över matrislängden så fortsätter funktionen vidare
-                if index1+2 > self.matrisstorlek-1 or index2+2 > self.matrisstorlek or i == 0 or i == 2:
+                if index1+2 > self.matrisstorlek-1 or index2+2 > self.matrisstorlek-1 or i == 0 or i == 2:
                     continue
-                höger_diagonal = bräda[index1+1][index2+1]
+                höger_diagonal = self.spelbräda[index1+1][index2+1]
                 # if satsen delar upp befordrade pjäser så att de inte multipliceras med 10, kanske gör en egen funktion för befordrade pjäser
                 if i<10:
                     if höger_diagonal != 0 and höger_diagonal != i and höger_diagonal != i*10:
@@ -165,41 +178,39 @@ class Spela(Spelbräda):
 
     def kolla_obligatoriska_drag_höger_upp(self):
             # index1 tillhör rad och index2 tillhör kolonn
-            for index1, a in enumerate (self.spelbräda.reverse()):
+            for index1, a in enumerate (self.spelbräda[::-1]):
                 for index2, i in enumerate (a):
                     #Try satsen ser till så att ifall index går över matrislängden så fortsätter funktionen vidare
-                    if index1+2 > self.matrisstorlek-1 or index2+2 > self.matrisstorlek or i == 0 or i == 1:
+                    if index1+2 > self.matrisstorlek-1 or index2+2 > self.matrisstorlek-1 or i == 0 or i == 1:
                         continue
-                    höger_diagonal = bräda[index1+1][index2+1]
+                    höger_diagonal = self.spelbräda[::-1][index1+1][index2+1]
                     # if satsen delar upp befordrade pjäser så att de inte multipliceras med 10, kanske gör en egen funktion för befordrade pjäser
                     if i<10:
                         if höger_diagonal != 0 and höger_diagonal != i and höger_diagonal != i*10:
-                            if self.spelbräda[index1+2][index2+2] == 0:
+                            if self.spelbräda[::-1][index1+2][index2+2] == 0:
                                 return True
                     else:
                         if höger_diagonal != 0 and höger_diagonal != i and höger_diagonal != i/10:
-                            if self.spelbräda[index1+2][index2+2] == 0:
+                            if self.spelbräda[::-1][index1+2][index2+2] == 0:
                                 return True
 
     def kolla_obligatoriska_drag_vänster_upp(self):
-        for index1,a in enumerate (self.spelbräda.reverse()):
+        for index1,a in enumerate (self.spelbräda[::-1]):
             for index2, i in enumerate (a):
                 # if satsen är för att index i listor kan bli mindre än noll och då räknar de istället upp ifrån höger sida
                 if index1+2 > self.matrisstorlek-1  or index2-2 < 0 or i == 0 or i == 1: 
                     continue
-                vänster_diagonal = self.spelbräda[index1+1][index2-1]
+                vänster_diagonal = self.spelbräda[::-1][index1+1][index2-1]
                 #if sats delar upp befordrade pjäser
                 if i<10:
                     if vänster_diagonal != 0 and vänster_diagonal != i and vänster_diagonal != i*10:
-                        if self.spelbräda[index1+2][index2-2] == 0:
+                        if self.spelbräda[::-1][index1+2][index2-2] == 0:
                             return True
                 else:
                     if vänster_diagonal != 0 and vänster_diagonal != i and vänster_diagonal != i/10:
-                        if self.spelbräda[index1+2][index2-2] == 0:
+                        if self.spelbräda[::-1][index1+2][index2-2] == 0:
                             return True
 
-
-#   def funktion som kollar ifall pjäs är befodrad
 
 def bokstavskollen(bokstav):
     while True:
@@ -275,12 +286,28 @@ def kolla_pjäser():
     #kolla diagonalitet
     pass
 
-def obligatoriska_drag():
-    höger_ner = bräda.kolla_obligatoriska_drag_höger_ner()
-    vänster_ner = bräda.kolla_obligatoriska_drag_vänster_ner()
-    höger_upp = bräda.kolla_obligatoriska_drag_höger_upp()
-    vänster_upp = bräda.kolla_obligatoriska_drag_vänster_upp()
-    values = list(höger_ner, vänster_ner, höger_upp, vänster_upp)
+def obligatoriska_drag(pjäs):
+    #input är ett värde antingen 1 eller 2 för att ange vilken spelares tur det är
+    #output är bool värde
+    typ_pjäs = bräda.spelbräda[pjäs[0]][pjäs[1]]
+    if  typ_pjäs == 1 or typ_pjäs ==10:
+        höger_ner = bräda.kolla_obligatoriska_drag_höger_ner()
+        vänster_ner = bräda.kolla_obligatoriska_drag_vänster_ner()
+        values = [höger_ner, vänster_ner]
+        if typ_pjäs == 10:
+            höger_upp = bräda.kolla_obligatoriska_drag_höger_upp()
+            vänster_upp = bräda.kolla_obligatoriska_drag_vänster_upp()
+            values.append(höger_upp, vänster_upp)
+
+    elif typ_pjäs == 2 or typ_pjäs == 20:
+        höger_upp = bräda.kolla_obligatoriska_drag_höger_upp()
+        vänster_upp = bräda.kolla_obligatoriska_drag_vänster_upp()
+        values = [höger_upp, vänster_upp]
+        if typ_pjäs == 20:
+            höger_ner = bräda.kolla_obligatoriska_drag_höger_ner()
+            vänster_ner = bräda.kolla_obligatoriska_drag_vänster_ner()
+            values.append(höger_ner, vänster_ner)
+            
     if any(values) == True:
         return True
     else:
@@ -290,7 +317,7 @@ def välj_pjäser_spelare1():
     # Input: inget Output: tvp listor av integers
     # Ber spelare 1 att välja först en pjäs att flytta och sedan vart den skall flyttas
     while True:
-        vald_pjäs = list(map(int,(bokstavskollen(input("Skriv position i ett nummer. Ex rad 2 och kolonn 4 blir 24: ")))))
+        vald_pjäs = [int(a) for a in (input("Välj en pjäs. Skriv vilken rad, kolonn: ")).split(",")]
         # for satsen gör om koordinaterna till en mindre för att hamna på rätt plats i matrisen
         for i in range(len(vald_pjäs)): vald_pjäs[i] = vald_pjäs[i]-1
         typ_pjäs = bräda.spelbräda[vald_pjäs[0]][vald_pjäs[1]]
@@ -301,9 +328,9 @@ def välj_pjäser_spelare1():
         elif typ_pjäs == 0:
             print("\nDet står ingen pjäs här: ")
             continue
-
-        vald_plats = list(map(int,(bokstavskollen(input("Skriv position i ett nummer. Ex rad 2 och kolonn 4 blir 24: ")))))
-        for i in range(len(vald_pjäs)): vald_pjäs[i] = vald_pjäs[i]-1
+        # detta är en upprepning av koden innan för att välja filken position användaren vill plytta pjäsen till
+        vald_plats = [int(a) for a in (input("Välj en plats. Skriv vilken rad, kolonn: ")).split(",")]
+        for i in range(len(vald_plats)): vald_plats[i] = vald_plats[i]-1
         typ_plats = bräda.spelbräda[vald_plats[0]][vald_plats[1]]
         if typ_plats != 0:
             print("\nDet står en pjäs på denna plats redan")
@@ -317,20 +344,20 @@ def välj_pjäser_spelare2():
     # Input: inget Output: två listor av integers
     # Ber spelare 2 att välja först en pjäs att flytta och sedan vart den skall flyttas
     while True:
-        vald_pjäs = list(map(int,(bokstavskollen(input("Skriv position i ett nummer. Ex rad 2 och kolonn 4 blir 24: ")))))
+        vald_pjäs = [int(a) for a in (input("Välj en  pjäs. Skriv vilken rad, kolonn: ")).split(",")]
         # for satsen gör om koordinaterna till en mindre för att hamna på rätt plats i matrisen
         for i in range(len(vald_pjäs)): vald_pjäs[i] = vald_pjäs[i]-1
         typ_pjäs = bräda.spelbräda[vald_pjäs[0]][vald_pjäs[1]]
         #Typ_pjäs anger vilken typ av pjäs vald_pjäs är i matrisen, den säger sedan att du inte får välja att röra motståndarens pjäser
         if typ_pjäs == 1 or typ_pjäs == 10:
-            print(f"\nDu kan bara välja pjäser av typ {bräda.symbol1}")
+            print(f"\nDu kan bara välja pjäser av typ {bräda.symbol2}")
             continue    
         elif typ_pjäs == 0:
             print("\nDet står ingen pjäs här: ")
             continue
 
-        vald_plats = list(map(int,(bokstavskollen(input("Skriv position i ett nummer. Ex rad 2 och kolonn 4 blir 24: ")))))
-        for i in range(len(vald_pjäs)): vald_pjäs[i] = vald_pjäs[i]-1
+        vald_plats = [int(a) for a in (input("Välj en plats. Skriv vilken rad, kolonn: ")).split(",")]
+        for i in range(len(vald_plats)): vald_plats[i] = vald_plats[i]-1
         typ_plats = bräda.spelbräda[vald_plats[0]][vald_plats[1]]
         if typ_plats != 0:
             print("\nDet står en pjäs på denna plats redan")
@@ -339,16 +366,12 @@ def välj_pjäser_spelare2():
             break
     return vald_pjäs, vald_plats
 
-def utför_drag(värde):
-    #utför olika typer av drag beroende på input, typ ta_pjäs höger_upp, vänster_upp, höger_ner, vänster_ner, eller flytta 
-    if värde == 0:
-        pass
-    elif värde == 1:
-        pass
-    elif värde == 2:
-        pass
-    elif värde == 3:
-        pass
+def utför_drag(pjäs, plats, attackerad_pjäs):
+    #utför olika typer av drag beroende på input, typ ta_pjäs höger_upp, vänster_upp, höger_ner, vänster_ner, eller flytta
+    if attackerad_pjäs == "":
+        bräda.flytta_pjäs(pjäs, plats)
+    else:
+        bräda.ta_pjäs(pjäs, plats, attackerad_pjäs)
 
 def befordra_pjäser():
     #går igenom spelplanen och befodrar pjäser
@@ -357,7 +380,7 @@ def befordra_pjäser():
             bräda.spelbräda[0][index] = 20
     for index, värde in enumerate (bräda.spelbräda[bräda.matrisstorlek - 1]):
         if värde == 1:
-            bräda.spelbräda[0][index] = 10
+            bräda.spelbräda[bräda.matrisstorlek - 1][index] = 10
 
 def huvudprogram():
     print("\nVälkommen till damspelet")
@@ -373,30 +396,43 @@ def huvudprogram():
         print(bräda)
         if index % 2 != 0:
             while True:
+                spelare = 1
                 pjäs, plats = välj_pjäser_spelare1()
-                if obligatoriska_drag == True:
-                    print("Det finns obligatoriska drag att göra")
-                    continue
                 if kolla_dragdiagonalitet(pjäs, plats) == False: #i ordning, 
                     continue
                 ta_pjäs, attackerad_pjäs = bräda.kolla_ta_pjäs(pjäs, plats)
-                if ta_pjäs == False and bräda.kolla_laglilg_sträcka_flytta(pjäs, plats) == False:
+                if obligatoriska_drag(pjäs) == True and ta_pjäs == True:
+                    index += 1
+                    pass
+                elif obligatoriska_drag(pjäs) == True and ta_pjäs == False:
+                    print("\nDet finns obligatoriska drag att göra")
                     continue
-                elif ta_pjäs == False and bräda.kolla_laglilg_sträcka_flytta(pjäs, plats) == True:
-                    kolla_drag = 
-
+                if ta_pjäs == False and bräda.kolla_laglilg_sträcka_flytta(pjäs, plats) == False:
+                    print("\nså får du inte göra, din lilla råtta: ")
+                    continue
+                if bräda.kolla_laglig_rikting(pjäs, plats) == False:
+                    print("\ndu kan inte röra dig i den riktningnen")
+                    continue
                 utför_drag(pjäs, plats, attackerad_pjäs)
                 vinst = kolla_vinst() #True eller False
                 break
         else:
             while True:
+                spelare = 2
                 pjäs, plats = välj_pjäser_spelare2()
-                if obligatoriska_drag == True:
+                if kolla_dragdiagonalitet(pjäs, plats) == False: #i ordning, 
+                    continue
+                ta_pjäs, attackerad_pjäs = bräda.kolla_ta_pjäs(pjäs, plats)
+                if obligatoriska_drag(pjäs) == True and ta_pjäs == True:
+                    index += 1
+                    pass
+                elif obligatoriska_drag(pjäs) == True and ta_pjäs == False:
                     print("Det finns obligatoriska drag att göra")
                     continue
-                if kolla_pjäser(pjäs, plats) == False:
+                if ta_pjäs == False and bräda.kolla_laglilg_sträcka_flytta(pjäs, plats) == False:
+                    print("så får du inte göra, din lilla råtta: ")
                     continue
-                utför_drag(pjäs, plats)
+                utför_drag(pjäs, plats, attackerad_pjäs)
                 vinst = kolla_vinst() #True eller False
                 break
             
